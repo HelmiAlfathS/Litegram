ad32ef0157e466d3fd48a244cabde560505206ae
HelmiAlfathS <helmialfathseptiana@gmail.com>
2020-08-01 10:06:49 -0700

    setfirebase, upload form, custom hook for firebase, progress bar, img card container

--- src/hooks/UseFirestore.js
+++ src/hooks/UseFirestore.js
@@ -1,28 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { projectFireStore } from '../firebase/config';
 
+// Custom Hook to grab/fetch data from firestore on collection of 'images' file
 export const UseFirestore = (collection) => {
-  // const [docs, setDocs] = useState([]);
-
-  // useEffect(() => {
-  //   const unsub = projectFireStore
-  //     .collection(collection)
-  //     .orderBy('createdAt', 'desc')
-  //     .onSnapshot((snap) => {
-  //       let documents = [];
-  //       snap.forEach((doc) => {
-  //         documents.push({ ...doc.data(), id: doc.id });
-  //       });
-  //       setDocs(documents);
-  //     });
-
-  //   return () => unsub();
-  //   // this is a cleanup function that react will run when
-  //   // a component using the hook unmounts
-  // }, [collection]);
-
-  // return { docs };
-
   const [docs, setDoc] = useState([]);
 
   useEffect(() => {
